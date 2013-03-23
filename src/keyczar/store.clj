87061bd3589dda8e4b7484d64aaaade1ebb55c61
Thomas Rampelberg <thomas@saunter.org>
2013-03-23 13:07:42 -0700

    Now with the ability to add new keys

--- src/keyczar/store.clj
+++ src/keyczar/store.clj
@@ -1,15 +1,27 @@
 (ns keyczar.store
   (:require [clojure.java.io :as io])
-  (:import (org.keyczar Keyczar KeyMetadata DefaultKeyType)
-    (org.keyczar.enums KeyPurpose)))
+  (:import (org.keyczar DefaultKeyType GenericKeyczar
+                        Keyczar KeyczarFileReader KeyMetadata)
+    (org.keyczar.enums KeyPurpose KeyStatus)))
+
+(defn add-key [store]
+  (.addVersion store (. KeyStatus PRIMARY))
+  (save store))
+
+(defn open [name]
+  (GenericKeyczar. (KeyczarFileReader. name)))
 
 (defn save [store]
-  (with-open [wrtr (io/output-stream (.getName store))]
-  (.write wrtr
-    (.. store toString (getBytes (. Keyczar DEFAULT_ENCODING))))))
+  (.write store (str (.. store getMetadata getName) "/")))
 
 (defn create [name]
-  (save (KeyMetadata.
-    name
-    (. KeyPurpose DECRYPT_AND_ENCRYPT)
-    (. DefaultKeyType RSA_PRIV))))
+  (.mkdirs (io/file name))
+  (with-open [wrtr (io/output-stream (str name "/" "meta"))]
+  (.write wrtr
+    (..
+      (KeyMetadata.
+        name
+        (. KeyPurpose DECRYPT_AND_ENCRYPT)
+        (. DefaultKeyType RSA_PRIV))
+      toString (getBytes (. Keyczar DEFAULT_ENCODING))))))
+
