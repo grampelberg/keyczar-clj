249e75a71b4cc1aa9085e1ee8a69b67ad8307576
Thomas Rampelberg <thomas@saunter.org>
2013-03-24 21:20:32 -0700

    import/export for a private key

--- src/keyczar/set/guard.clj
+++ src/keyczar/set/guard.clj
@@ -1,6 +1,22 @@
 (ns keyczar.set.guard
   (:require [keyczar.set :as set])
-  (:import (org.keyczar Crypter)))
+  (:import (org.keyczar Crypter GenericKeyczar PkcsKeyReader)
+    (org.keyczar.enums KeyPurpose RsaPadding)
+    (java.io ByteArrayInputStream)))
+
+; (defn import [name passphrase]
+;   )
+
+(defn from-cert [cert passphrase]
+  (GenericKeyczar. (PkcsKeyReader.
+    (. KeyPurpose DECRYPT_AND_ENCRYPT)
+    (ByteArrayInputStream. (.getBytes cert))
+    (. RsaPadding OAEP)
+    passphrase)))
+
+(defn export [name passphrase]
+  (let [source (set/open name)]
+    (.getPemString (.getPrimaryKey source) passphrase)))
 
 (defn open [name]
   (Crypter. (set/reader name)))
